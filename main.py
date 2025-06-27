c38870cefad11812dbf43eeb78eb0c878ecd8941
kharbak <kharbak@amazon.com>
2025-06-27 11:32:55 -0700

    Fix TypeError by converting string to int

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 def add_numbers(a, b):
+    if isinstance(b, str):
+        b = int(b)
     return a + b
 
 print(add_numbers(1, 2))
-print(add_numbers(1, "2"))  # This will cause a TypeError
+print(add_numbers(1, "2"))  # This will now work correctly
